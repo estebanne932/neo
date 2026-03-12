a701b9d17fdeb6b4c20da0554268813eacb303fa
Fernanda Estebanne <fernandaestebanne@gmail.com>
2026-03-11 18:34:10 -0600

    actualizacion del sitio

--- script.js
+++ script.js
@@ -42,3 +42,21 @@ function typeEffect() {
 }
 
 typeEffect();
+
+const slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+setInterval(() => {
+
+  slides[index].classList.remove("active");
+
+  index++;
+
+  if(index >= slides.length){
+    index = 0;
+  }
+
+  slides[index].classList.add("active");
+
+}, 3500);
